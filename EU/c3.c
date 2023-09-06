0391533a4bf09924f5b4da8a42400fbd02dae16c
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-06 17:17:03 -0300

    Create c3.c

---
+++ EU/c3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+  int a, b, c, res;
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &a); 
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &b); 
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &c); 
+  
+  res = a * b * c;
+  
+  printf("O valor da soma = %d\n", res);
+  
+
+}
