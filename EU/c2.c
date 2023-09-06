19e4a9812bfd245e74fc46c2d780204ab332fae8
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-06 17:14:57 -0300

    Create c2.c

---
+++ EU/c2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+
+  int a, b, soma;
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &a); 
+  
+  printf("Digite um numero inteiro: ");
+  scanf("%d", &b); 
+  soma = a + b;
+  
+  printf("O valor da soma = %d\n", soma);
+}
