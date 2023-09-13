c0a0b9afef5ce5776e3208801dc4a7cd7c81811e
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:05:29 -0300

    Update atv4.c

--- EU/atv4.c
+++ EU/atv4.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	
+	float n1, n2, resultado;
+	
+	printf("Entre com o primeiro valor:");
+	scanf("%f", &n1);
+	
+	printf("Entre com o segundo valor:");
+	scanf("%f", &n2);
+	
+	resultado = n1 + n2;
+	
+	printf("O resultado da soma de %f e %f é %f", n1,n2,resultado);
+}
