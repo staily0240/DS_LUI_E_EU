f97d3c9003947e7fe876adfe2dca19c90073a9ca
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:03:11 -0300

    Update atv2.c

--- EU/atv2.c
+++ EU/atv2.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	
+	int n1, n2, resultado;
+	
+	printf("Digite o primeiro valor:");
+	scanf("%d", &n1);
+	
+	printf("Digite o segundo valor:");
+	scanf("%d", &n2);
+	
+	resultado = n1 + n2;
+	
+	printf("O resultado da soma de %d e %d é %d", n1,n2,resultado);
+}
