61abc01b991645fe92efd7cef64064e3087f86e1
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:09:53 -0300

    Update atv8.c

--- EU/atv8.c
+++ EU/atv8.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+
+	int n1, n2, r1, r2;
+	
+	printf("Entre com o primeiro valor:");
+	scanf("%d", &n1);
+	
+	printf("Entre com o segundo valor:");
+	scanf("%d", &n2);
+	
+	r1 = n1/2;
+	r2 = n2*3;
+	
+	printf("Resultados:\n %d / 2 = %d \n%d * 3 = %d", n1,r1,n2,r2);
+}
