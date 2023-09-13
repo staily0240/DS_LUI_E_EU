32a4f204f7944d681d9087f0375d3accf340ab49
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:06:46 -0300

    Update atv5.c

--- EU/atv5.c
+++ EU/atv5.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	
+	float n1, n2, n3, resultado;
+	
+	printf("Entre com o primeiro valor:");
+	scanf("%f", &n1);
+	
+	printf("Entre com o segundo valor:");
+	scanf("%f", &n2);
+	
+	printf("Entre com o terceiro valor:");
+	scanf("%f", &n3);
+	
+	resultado = (n1 + n2 + n3)/3;
+	
+	printf("A média dos números %f, %f e %f é %f", n1,n2,n3,resultado);
+}
