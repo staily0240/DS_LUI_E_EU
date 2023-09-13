73f09705e764267b5376064c9f625454ae44683a
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:04:15 -0300

    Update atv3.c

--- EU/atv3.c
+++ EU/atv3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	
+	int n1, n2, n3, resultado;
+	
+	printf("Digite o primeiro valor:");
+	scanf("%d", &n1);
+	
+	printf("Digite o segundo valor:");
+	scanf("%d", &n2);
+	
+	printf("Digite o terceiro valor:");
+	scanf("%d", &n3);
+	
+	resultado = n1 * n2 * n3;
+	
+	printf("O resultado da multiplicação de %d, %d e %d é %d", n1,n2,n3,resultado);
+}
