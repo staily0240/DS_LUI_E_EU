3e935ce8bfb3eb5151aab485779dcbd0deb03be6
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:02:00 -0300

    Update atv1.c

--- EU/atv1.c
+++ EU/atv1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(int argc, char *argv[]) {
+	
+	int valor;
+	printf("Digite o valor:");
+	scanf("%d", &valor);
+	
+	printf("O valor é %d, valor");
+}
