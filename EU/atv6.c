f15ff2bbb005b5496e34aec8856d54f1fc4cdb8e
staily0240 <99103595+staily0240@users.noreply.github.com>
2023-09-13 08:07:38 -0300

    Update atv6.c

--- EU/atv6.c
+++ EU/atv6.c
@@ -1,1 +1,12 @@
+int main(int argc, char *argv[]) {
 
+	int num;
+	
+	printf("Entre com um valor: \n");
+	scanf("%d",&num);
+	
+	num++;
+	
+	printf("O suceessor do número informado é: %d\n", num);
+	
+}
